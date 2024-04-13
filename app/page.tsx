e4bf62eea7c17ce30e6d55d23eaeeae01dd1c527
mikieyx <mikieyx@gmail.com>
2024-04-13 13:16:03 -0700

    added form input & button

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 export default function Chat() {
   return (
     <div className="flex w-full min-h-screen items-center justify-center flex-col">
-      <div>
-
-      </div>
-      <input>
-      </input>
       
+      <div>
+        <div className="bg-gray-600 w-full"></div>
+        <form className="inline-block">
+          <input type="text" placeholder="Type here" className="input w-full max-w-xs bg-gray-700 rounded p-2" />
+          <button className="btn btn-primary">Ask</button>
+        </form>
+        
+      </div>
     </div>
   );
 }
