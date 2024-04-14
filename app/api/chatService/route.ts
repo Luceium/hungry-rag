f090edb9a7bfe425a1e87ae3c06f7603149c1b97
Mau <79354255+Luceium@users.noreply.github.com>
2024-04-13 20:37:08 -0700

    fixes reddis

--- app/api/chatService/route.ts
+++ app/api/chatService/route.ts
@@ -2,9 +2,23 @@ import { redirect } from "next/dist/server/api-utils";
 import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "redis";
 
-// const client = createClient({ url: "redis://10.0.0.95:6379" });
-// client.on("error", (err) => console.log("Redis Client Error", err));
-// client.connect();
+interface VectaraResponse {
+  responseSet: [
+    {
+      response: [];
+      status: [];
+      document: [];
+      summary: [];
+      futureId: number;
+    }
+  ];
+  status: [];
+  metrics: any;
+}
+
+const client = createClient({ url: "redis://default:luceium@10.0.0.95:6379" });
+client.on("error", (err) => console.log("Redis Client Error", err));
+client.connect();
 
 export async function GET(request: NextRequest) {
   const query = request.nextUrl.searchParams.get("query");
@@ -13,25 +27,25 @@ export async function GET(request: NextRequest) {
   }
 
   //wait until redis is connected
-//   await client.connect();
 
-//   // check if redis has the query
-//   const redisResponse = await queryRedis(query);
-//   if (redisResponse) {
-//     return Response.json({ data: redisResponse });
-//   }
+  // check if redis has the query
+  const redisResponse = await queryRedis(query);
+  console.log("redisResponse " + redisResponse);
+  if (redisResponse) {
+    return Response.json({ data: redisResponse });
+  }
 
   const vectaraResponse = await queryVectara(query);
-
-  //updateRedis(query, vectaraResponse);
+  console.log("vectaraResponse ", vectaraResponse);
+  // updateRedis(query, vectaraResponse);
 
   return Response.json({ data: vectaraResponse });
 }
 
-// async function queryRedis(query: string): Promise<string | false> {
-//   const value = await client.get("key");
-//   return value ?? false;
-//}
+async function queryRedis(query: string): Promise<string | false> {
+  const value = await client.get(query);
+  return value ?? false;
+}
 
 async function queryVectara(query: string): Promise<string> {
   let data = JSON.stringify({
@@ -48,7 +62,7 @@ async function queryVectara(query: string): Promise<string> {
         },
         corpusKey: [
           {
-            corpus_id:2,
+            corpus_id: 2,
           },
         ],
         summary: [
@@ -75,11 +89,11 @@ async function queryVectara(query: string): Promise<string> {
   };
   const res = await fetch("https://api.vectara.io/v1/query", config);
   const result = await res.json();
-  console.log(result)
+  console.log("result ", result);
 
-  return "";
+  return result;
 }
 
-// async function updateRedis(query: string, response: string) {
-//   await client.set(query, response);
-// }
+async function updateRedis(query: string, response: string) {
+  await client.set(query, response);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,9 @@ import Input from "@/components/input";
 import Response from "@/components/response";
 import { useState } from "react";
 
-
 export default function Chat() {
   const [response, setResponse] = useState("");
-  
+
   return (
     <div className="flex w-full min-h-screen items-center justify-center flex-col">
       <div>
